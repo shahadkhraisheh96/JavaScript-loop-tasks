a1f2efe8604e69795a4d7571b23b695248ad896f
shahadkhraisheh96 <shahadkh2004@gmail.com>
2026-03-13 00:17:38 +0300

    first 3 task

---
+++ loop-task.js
@@ -0,0 +1,39 @@
+//Create a script that displays numbers from 1 to 10 on one line separated by a dash (-).
+
+for(let i=0;i<=10;i++){
+    document.writeln(i+'-');
+}
+document.writeln("<br>");
+
+
+//Create a script using a for loop to add all integers between 0 and 30 and display the total.
+let total2=0;
+for(let i=0;i<=30;i++){
+    total2=total2+i;
+}
+document.writeln(`<p>total2 = ${total2}</p>`);
+document.writeln("<br>");
+
+//Write a program that prints all even numbers from 1 to 50 using a loop.
+for(let i=1;i<=50;i++){
+    if(i%2==0){
+        document.writeln(i+" ");
+    }
+}
+
+document.writeln("<br>");
+
+//Write a program that prints numbers from 10 down to 1 using a loop.
+for(let i=10;i>=1;i--){
+    document.writeln(i+" ");
+}
+document.writeln("<br>");
+
+//Create a script to generate the following pattern using nested for loops.
+for(let row=0;row<=4;row++){
+    
+}
+
+
+
+
