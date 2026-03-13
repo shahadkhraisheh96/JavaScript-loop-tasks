5469e4d67a75550546402f7caad8865270e1dd5a
shahadkhraisheh96 <shahadkh2004@gmail.com>
2026-03-13 03:42:22 +0300

    update the first 10

--- loop-task.js
+++ loop-task.js
@@ -21,19 +21,96 @@ for(let i=1;i<=50;i++){
     }
 }
 
-document.writeln("<br>");
+document.writeln("<br><br>");
 
 //Write a program that prints numbers from 10 down to 1 using a loop.
 for(let i=10;i>=1;i--){
     document.writeln(i+" ");
 }
-document.writeln("<br>");
+
+document.writeln("<br><br>");
 
 //Create a script to generate the following pattern using nested for loops.
-for(let row=0;row<=4;row++){
+const size1=5;
+for(let row=0;row<size1;row++){
+let line="";
+    for(let column=0;column<size1;column++){
+        if(column<size1-row-1){
+            line+="A";
+        }else{
+            let char=String.fromCharCode(65+row);
+            line+=char+" ";
+        }
+    }
+    document.writeln(line +"<br>");
     
 }
+document.writeln("<br><br>");
 
+//6
+const size2=5;
+for(let row=1;row<=size1;row++){
+let line="";
+    for(let column=1;column<=size1;column++){
+        if(column<=size1-row){
+            line+="1";
+        }else{
+           
+            line+=row+" ";
+        }
+    }
+    document.writeln(line +"<br>");
+    
+}
+document.writeln("<br><br>");
 
+//7
 
+const size3=5;
+for(let row=1;row<=size1;row++){
+let line="";
+    for(let column=1;column<=size1;column++){
+        if(row==column){
+            line+=row;
+        }else{
+            line+="0";
+        }
+    }
+    document.writeln(line +"<br>");
+    
+}
+document.writeln("<br><br>");
+
+//8
+let factorialnum=1;
+for(let i=1;i<=5;i++){
+    factorialnum =factorialnum*i;
+}
+document.writeln(factorialnum);
+
+document.writeln("<br><br>");
+
+//9Write a program to print the Fibonacci sequence using a loop.
+let num1=0;
+let num2=1;
+let fnum=9;
+let Fibonacci=0;
+for(let i=0;i<fnum;i++){
+    document.writeln(num1+" ");
+    factorialnum=num1+num2;
+    num1=num2;
+    num2=factorialnum;
+}
+document.writeln("<br><br>");
+
+//10 Write a program that counts the number of "c" characters in the following 
+const text="Orange Coding School";
+const charToCount='c';
+let charcount=0;
+for(let i=0;i<=text.length-1;i++){
+    if(text[i].toLowerCase()===charToCount)
+    charcount++;
+}
+document.writeln("count of c in Orange Coding School="+charcount);
+document.writeln("<br><br>");
 
